16328765efd35d68d3dcda8cb9652747affc2bc5
P.Ivo <pivolluz@gmail.com>
2023-03-02 21:50:37 -0300

    criando projeto mongoDb

---
+++ src/test/java/com/PIvo/project/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PIvo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
