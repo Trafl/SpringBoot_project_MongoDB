a834a2c285fb70bd8e57c4cd750d0a7a92730fdf
P.Ivo <pivolluz@gmail.com>
2023-03-03 20:54:23 -0300

    padrão DTO para retorna usuarios

--- src/main/java/com/PIvo/project/resoucers/UserResource.java
+++ src/main/java/com/PIvo/project/resoucers/UserResource.java
@@ -1,6 +1,7 @@
 package com.PIvo.project.resoucers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PIvo.project.domain.User;
+import com.PIvo.project.dto.UserDTO;
 import com.PIvo.project.services.UserService;
 
 @RestController
@@ -19,9 +21,9 @@ public class UserResource {
 	private UserService service;
 	
     @GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
