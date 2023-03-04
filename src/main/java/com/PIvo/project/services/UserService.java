b52d9da66c92cd0d911c152efc499aceb2b45a12
P.Ivo <pivolluz@gmail.com>
2023-03-03 21:58:52 -0300

    Inserção de usuario por id

--- src/main/java/com/PIvo/project/services/UserService.java
+++ src/main/java/com/PIvo/project/services/UserService.java
@@ -1,12 +1,14 @@
 package com.PIvo.project.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.PIvo.project.UserRepository;
 import com.PIvo.project.domain.User;
+import com.PIvo.project.services.exeption.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
