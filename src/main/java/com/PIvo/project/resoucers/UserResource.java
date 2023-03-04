c4b5ab70cb17adf689665ccf0aaa792799b92cc3
P.Ivo <pivolluz@gmail.com>
2023-03-04 17:43:35 -0300

    atualização de usuario com PUT

--- src/main/java/com/PIvo/project/resoucers/UserResource.java
+++ src/main/java/com/PIvo/project/resoucers/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,31 +26,39 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
-    @GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
+
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-    
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> findById(@PathVariable String id){
-    	 User obj = service.findById(id);
-    	 return ResponseEntity.ok().body(new UserDTO(obj));
-    }
-    
-    @PostMapping
-    public ResponseEntity<User> insert(@RequestBody UserDTO objDTO){
-    	 User user = service.fromDTO(objDTO);
-    	 user = service.insert(user);
-    	 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
-    	 return ResponseEntity.created(uri).body(user);
-    }
-    
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable String id){
-    	 service.delete(id);
-    	 return ResponseEntity.noContent().build();
-    }
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody UserDTO objDTO) {
+		User user = service.fromDTO(objDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id ) {
+		User user = service.fromDTO(objDTO);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/PIvo/project/services/UserService.java
+++ src/main/java/com/PIvo/project/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newUser = findById(obj.getId());
+		updateData(newUser, obj);
+		return repository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User obj) {
+		newUser.setName(obj.getName());
+		newUser.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO (UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
