ac004c318fe0a2c40b50dfb184f42f402d81e346
P.Ivo <pivolluz@gmail.com>
2023-03-05 13:42:27 -0300

    obtendo um post por id

---
+++ src/main/java/com/PIvo/project/services/PostService.java
@@ -0,0 +1,22 @@
+package com.PIvo.project.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.PIvo.project.domain.Post;
+import com.PIvo.project.repository.PostRepository;
+import com.PIvo.project.services.exeption.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
