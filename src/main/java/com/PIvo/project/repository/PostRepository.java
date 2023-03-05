a4d906c3fc38e6bd15749448d48946dd3b4a52ba
P.Ivo <pivolluz@gmail.com>
2023-03-05 20:39:53 -0300

    consulta simples com query methods

--- src/main/java/com/PIvo/project/repository/PostRepository.java
+++ src/main/java/com/PIvo/project/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.PIvo.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.PIvo.project.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post>findByTitleContainingIgnoreCase(String text);
 }
